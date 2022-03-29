cb66539fd75a3aa40c399cb47d824322554c83e9
luksouza <apolloboficial@gmail.com>
2022-03-29 14:30:10 -0300

    Criando o mesmo server com o express

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+    res.send("<h1>Pagina Home</h1>")
+})
+
+app.get('/tecnologia', function(req, res){
+    res.send("<h1>Pagina Tecnologia</h1>")
+})
+
+app.get('/contato', function(req, res){
+    res.send("<h1>Pagina de contato</h1>")
+})
+app.listen(4100, x => console.log({ Status: "Server rodando com express", PORT: 4100 }))
