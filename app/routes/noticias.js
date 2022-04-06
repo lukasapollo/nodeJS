c73ceecdf3cf56e34dcb0008ca366fb5d7c2ba3d
luksouza <apolloboficial@gmail.com>
2022-04-06 10:02:11 -0300

    Usando o MVC temos o nosso model que ficara responsavel por melhorar nossas consultas ao DB

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,12 +1,17 @@
-module.exports = (app) => {
-    app.get('/noticias', function(req, res){
+module.exports = (application) => {
+    application.get('/noticias', function(req, res){
 
-        const connection = app.config.dbConnection();
+        // Um detalhe do consign é que vamos acessar os diretorios de rota
+        // como uma propriedade do objeto que fica na variavel aplicattion.
+        // Obs: por questões de redundancia passou-se a chamar o {app} de {application}
+        const connection = application.config.dbConnection();
+        const noticiasModels = application.app.models.noticiasModel;
 
-        connection.query('select * from noticias', (error, result) => {
+        noticiasModels.getNoticias(connection, (error, result) => {
             res.render("noticias/noticias", { 
                 noticias : result 
             });
+
             console.log(result);
         })
     })
